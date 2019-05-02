57010d0c16317d502cb7049aec1bbdb8f867d1f2
CEDARBARN\JonA <jon.abbott@jppuk.net>
2019-05-02 10:29:56 +0100

    common log, with different types

--- AcTestFramework/FileLogger.cs
+++ AcTestFramework/FileLogger.cs
@@ -8,15 +8,14 @@ namespace Jpp.AcTestFramework
     {
         public enum LogType { Client, Server }
 
-        private const string FILE_NAME = "AcTests_[type].log";
+        private const string FILE_NAME = "AcTests.log";
         private readonly Logger _logger;
         private readonly bool _shouldLog;
 
         public FileLogger(string directory, LogType type, bool shouldLog = false)
         {
             _shouldLog = shouldLog;
-            var fileName = FILE_NAME.Replace("[type]", type.ToString());
-            var filePath = Path.Combine(directory ?? throw new InvalidOperationException(), fileName);
+            var filePath = Path.Combine(directory ?? throw new InvalidOperationException(), FILE_NAME);
 
             var config = new NLog.Config.LoggingConfiguration();
             var logfile = new NLog.Targets.FileTarget(type.ToString()) { FileName = filePath, KeepFileOpen = false, ArchiveAboveSize = 1000000, MaxArchiveFiles = 10 };
